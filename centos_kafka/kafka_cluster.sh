adca3af88bfa2a2cc2473cd97dcacb6b44ff63fa
lianshufeng <251708339@qq.com>
2018-07-01 16:11:56 +0800

    update
    Former-commit-id: 374abd56739103aa39ec61e90b4d446c5311b566

--- centos_kafka/kafka_cluster.sh
+++ centos_kafka/kafka_cluster.sh
@@ -3,22 +3,30 @@ if [ -z $CLUSTER_HOST ]; then
 	echo localhost
 else
 	CLUSTER_HOST=(${CLUSTER_HOST//,/ })
+	zookeeperConnect=""
 	i=0
 	for host in ${CLUSTER_HOST[@]}
 	do
+		zookeeperConnect=$zookeeperConnect""$host":2181,"
+		#### zookeeper 集群
 		let i++
-		#写入zk
+		echo "initLimit=10" >> $KAFKA_HOME/config/zookeeper.properties
+		echo "syncLimit=5" >> $KAFKA_HOME/config/zookeeper.properties
 		echo "server.$i=$host:2888:3888" >> $KAFKA_HOME/config/zookeeper.properties
 		#判断当前ip
 		ipexist=$(ip a | grep $host )
 		if [[ -z $ipexist ]] ;then
 			echo $ipexist
 		else 
+			mkdir /tmp/zookeeper
 			#取出对应的序号
-			echo "$i" > /tmp/ip
+			echo "$i" > /tmp/zookeeper/myid
+			#kafka broker.id
+			sed -i "s/broker.id=0/broker.id=$i/g" $KAFKA_HOME/config/server.properties
 		fi
-		
-	done 
+	done
+	#### kafka  集群
+	sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=$zookeeperConnect/g" $KAFKA_HOME/config/server.properties
 fi
 #防止多次被执行
-#echo "" > $0
+echo "" > $0
