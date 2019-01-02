43fb81b79c8e06167822cdae9881e8fe00405f1c
lianshufeng <251708339@qq.com>
2019-01-02 17:25:11 +0800

    提交代码
    Former-commit-id: d7722df4d02f450f91132d4ace4e5471da3a3850

--- native/install_docker.sh
+++ native/install_docker.sh
@@ -22,8 +22,12 @@ startDocker(){
 
 #安装docker
 installDocker(){
+	#安装 docker
 	yum install docker -y
 	chkconfig docker on
+	#安装 docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
 }
 
 #安装释放内存
