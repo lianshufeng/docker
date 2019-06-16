51648b47c56c6ef7920ce5122bb8b9d131460061
lianshufeng <251708339@qq.com>
2019-06-16 11:22:56 +0800

    升级docker ce的版本
    Former-commit-id: b1cfcc30c81c263c48ec0d3b91e891f71bbe96d5

--- native/install_docker.sh
+++ native/install_docker.sh
@@ -23,7 +23,15 @@ startDocker(){
 #安装docker
 installDocker(){
 	#安装 docker
-	yum install docker -y
+	#yum install docker -y
+	#卸载
+	yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine	
+	#设置稳定库	
+	yum install -y yum-utils device-mapper-persistent-data lvm2
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	#安装docker
+	yum install -y docker-ce docker-ce-cli containerd.io
+	#设置自动启动
 	chkconfig docker on
 	#安装 docker-compose
 	sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
