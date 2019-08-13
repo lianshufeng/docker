efdc38648f5091ef4e96d55b9f742a8c108dcc9c
lianshufeng <251708339@qq.com>
2019-08-13 09:55:24 +0800

    增加 kubernates 一键安装脚本
    Former-commit-id: fd1949f853d7f526b4115e5f48793d45b1167c90

--- native/install_k8s.sh
+++ native/install_k8s.sh
@@ -13,35 +13,35 @@ install_docker(){
 
 #安装k8s
 install_k8s(){
-	cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-	[kubernetes]
-	name=Kubernetes
-	# 阿里源
-	baseurl=http://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+# 阿里源
+baseurl=http://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 
-	enabled=1
-	gpgcheck=1
-	repo_gpgcheck=1
-	gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
-	EOF
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
 
-	# Set SELinux in permissive mode (effectively disabling it)
-	setenforce 0
-	sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-	yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-	systemctl enable --now kubelet
+systemctl enable --now kubelet
 }
 
 
 #设置防火墙
 set_Firewall(){
-	cat <<EOF >  /etc/sysctl.d/k8s.conf
-	net.bridge.bridge-nf-call-ip6tables = 1
-	net.bridge.bridge-nf-call-iptables = 1
-	EOF
-	sysctl --system
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
 }
 
 
