9e4a2bcb8ac5f74e1a82cc75e13f7c318a4b895b
lianshufeng <251708339@qq.com>
2019-08-15 22:36:17 +0800

    修改阿里源
    Former-commit-id: 150fa7ad678a9f7036a1f384cbec1c47605a80d8

--- native/update_docker.sh
+++ native/update_docker.sh
@@ -9,7 +9,10 @@ install_docker_ce(){
 	yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine	
 	#设置稳定库	
 	yum install -y yum-utils device-mapper-persistent-data lvm2
-	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	#设置源
+	# yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	#设置阿里源
+	yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 	#安装docker
 	yum install -y docker-ce docker-ce-cli containerd.io
 	
