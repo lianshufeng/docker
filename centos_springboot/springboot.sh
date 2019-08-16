3531ae2ee6bfcbd495929b060f45ac5acf17e8c2
lianshufeng <251708339@qq.com>
2019-08-16 18:58:07 +0800

    修改springboot的项目，支持自动拉去外部资源
    Former-commit-id: 6c88bde037e7fdc47b21eb38e4cdb90beba32e0f

---
+++ centos_springboot/springboot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+downloadResources(){
+	url=$1
+	foreUpdate=$2
+	
+	#文件名
+	fileName=${url##*/} 
+	
+	#缓存与强制更新的判断
+	if [ ! -f "$fileName" ]||[[ "$foreUpdate" == true ]] ; then
+		echo "update:" $url
+		curl $url -o $fileName
+	else
+		echo "cache:" $fileName
+	fi
+	
+}
+
+
+
+#更新资源
+downResources(){
+	urls=$1
+	urls=(${urls//,/ }) 
+	foreUpdate=$2
+
+	for url in ${urls[@]} 
+	do 
+		downloadResources $url $foreUpdate
+	done
+
+}
+
+
+#下载依赖资源
+loadResources(){
+	downResources $CacheResources false
+	downResources $UpdateResources true
+}
+
+
+
+
+
+
+
+
+loadResources
