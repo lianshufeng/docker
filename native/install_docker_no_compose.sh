3bed2fece8b21eb5d70f4cf58a93a07664b22ffa
lianshufeng <251708339@qq.com>
2019-08-21 10:41:48 +0800

    修改docker 更新脚本
    Former-commit-id: be73707d77765243b55baf2ef9f31931db653527

--- native/install_docker_no_compose.sh
+++ native/install_docker_no_compose.sh
@@ -22,8 +22,9 @@ startDocker(){
 
 #安装docker
 installDocker(){
-	#更新 docker
-	#卸载
+	#安装 docker
+	yum install docker -y
+	#卸载与更新
 	yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine	
 	#设置稳定库	
 	yum install -y yum-utils device-mapper-persistent-data lvm2
@@ -33,11 +34,6 @@ installDocker(){
 	yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 	#安装docker
 	yum install -y docker-ce docker-ce-cli containerd.io
-	
-	
-	
-	
-	
 	
 	#设置自动启动
 	chkconfig docker on
