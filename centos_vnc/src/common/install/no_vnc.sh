795419df06a89d925ee79b19055e9aa102215c13
lianshufeng <251708339@qq.com>
2024-03-26 12:06:05 +0800

    update

--- centos_vnc/src/common/install/no_vnc.sh
+++ centos_vnc/src/common/install/no_vnc.sh
@@ -3,9 +3,9 @@ set -u
 
 echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p $NO_VNC_HOME/utils/websockify
-wget -qO- https://github.com/novnc/noVNC/archive/v1.2.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
+wget -qO- https://github.com/novnc/noVNC/archive/v1.4.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
-wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz| tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
+wget -qO- https://github.com/novnc/websockify/archive/v0.11.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 ## create index.html to forward automatically to `vnc_lite.html`
 ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
@@ -13,5 +13,5 @@ ln -s $NO_VNC_HOME/vnc_lite.html $NO_VNC_HOME/index.html
 
 
 #安装python2
-yum install -y python2
-ln -s /usr/bin/python2 /usr/bin/python
+yum install -y python3
+ln -s /usr/bin/python3 /usr/bin/python
